11d1dec4c4ab3e8b9b29ad15896190d30ba72188
R. Frega <robertofrega@gmail.com>
2019-11-13 14:49:32 -0300

    style(dedup): deduplicando código reutilizavel do cadastro

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -13,44 +13,8 @@ import Container from "@material-ui/core/Container"
 
 import { Auth } from "aws-amplify"
 import { navigate } from "gatsby"
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://vicente.app/">
-        Vicente
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  )
-}
-
-const useStyles = makeStyles(theme => ({
-  "@global": {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
+import { Copyright } from "../components/copyright"
+import { useStyles } from "../styles/styles"
 
 export default function SignUp() {
   const classes = useStyles()
